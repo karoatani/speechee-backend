f1f482bf0824c0b7b8184065a104777570180771
karoatani <karoatani@gmail.com>
2024-12-03 07:57:46 +0100

    added

--- speechee/settings.py
+++ speechee/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+print(DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
